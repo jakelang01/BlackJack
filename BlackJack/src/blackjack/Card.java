aba03b4e9b386847b5e79c2a2aeb5fae7b7420a6
lang1090 <lang1090@umn.edu>
2023-08-02 14:06:21 -0500

    First commit

---
+++ BlackJack/src/blackjack/Card.java
@@ -0,0 +1,8 @@
+package blackjack;
+
+public class Card {
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ BlackJack/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Jake
+ *
+ */
+module BlackJack {
+}
