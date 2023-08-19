1de3f903462339479ca81bb5832108e361cdbde6
jakelang01 <jakelangenfeld1@gmail.com>
2023-08-19 00:03:52 -0500

    added scanner

--- BlackJack/src/game/BlackJack.java
+++ BlackJack/src/game/BlackJack.java
@@ -1,9 +1,12 @@
 package game;
 
+import java.util.Scanner;
+
 public class BlackJack {
 
 	public static void main(String[] args) {
 		Game game = new Game();
+		Scanner scan = new Scanner(System.in);
 
 		while (game.getShoe().collectionSize() > 15) { // play while there are more than 15 cards left in the shoe
 			game.deal();
